5136cb696dbf46031863b8de357d589e2f7faba9
valerydluski <valerydluski@gmail.com>
2020-06-13 19:56:40 +0300

    init: start task rslang

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import SpeakITStartPage from "./components/SpeakITStartPage";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <SpeakITStartPage />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
