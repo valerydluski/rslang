985d0721bb28fd90071efb0a2c9c40bf45e2f459
Artsemi Shareika <artsemishareika@gmail.com>
2020-06-20 14:57:40 +0300

    feat: add exit button, basic layout for savannah

---
+++ src/components/Navigation/NavigationExit.jsx
@@ -0,0 +1,12 @@
+/* import React from 'react';
+import '/work/final project/rslang/src/pages/games/Savannah/style.css';
+import {NavLink} from 'react-router-dom';
+
+{ /* const ExitButtonComponent = () => (
+    <div>
+      <NavLink to="/"><button className="exit-button">Exit</button></NavLink>
+    </div>
+    )
+*/}
+/*
+    export default ExitButtonComponent;
--- src/pages/games/Savannah/Savannah.jsx
+++ src/pages/games/Savannah/Savannah.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import '../Savannah/style.css';
+import styled, { keyframes } from 'styled-components';
+import { slideInDown } from 'react-animations';
 
-const Savannah = () => (
-  <div className="savannah_container">
-    <h1>Savannah</h1>
+
+const Bounce = styled.div`animation: 3s ${keyframes`${slideInDown}`}`;
+
+const SavannahComponent = (props) => (
+  <div>
+    <p>
+        Word : {props.name}
+      </p>
   </div>
 );
 
+
+class Savannah extends React.Component {
+
+  state = {
+    gameStarted: false
+  }
+
+  reload() {
+    console.log('again');
+  }
+
+  render() {
+    const divStyle = {
+      textAlign: 'center'
+    }
+
+return (
+  <div className="savannah_container">
+    <h1>Savannah</h1>
+    <Bounce onAnimationEnd={this.reload}>
+    <div className="english_word">
+      <SavannahComponent name = {'Word to learn'}/>
+      </div>
+      </Bounce>
+      <div className="game_words">
+    <SavannahComponent name = {'translation'}/>
+    <SavannahComponent />
+    <SavannahComponent />
+    <SavannahComponent />
+    </div>
+    
+    </div>
+)
+
+}
+}
 export default Savannah;
