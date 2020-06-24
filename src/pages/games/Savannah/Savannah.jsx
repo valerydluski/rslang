b2f1dcf563f202f143f3a403baf2e44f6bee0c3e
Artsemi Shareika <artsemishareika@gmail.com>
2020-06-24 18:13:34 +0300

    feat: add gameMode, key events, animations

--- src/pages/games/Savannah/Savannah.jsx
+++ src/pages/games/Savannah/Savannah.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import '../Savannah/style.css';
 import styled, { keyframes } from 'styled-components';
 import { slideInDown } from 'react-animations';
@@ -9,11 +10,32 @@ const Bounce = styled.div`animation: 3s ${keyframes`${slideInDown}`}`;
 const SavannahComponent = (props) => (
   <div>
     <p>
-        Word : {props.name}
+        {props.num}. Word : {props.name}
       </p>
   </div>
 );
+const extendLoad = () => (
+    ReactDOM.render(element, document.getElementById('first'))
+)
 
+document.addEventListener('keydown', (event) => {
+  if (event.key > 0 && event.key < 5) {
+  console.log(event.key);
+  }
+})
+
+const element = 
+    <div className = 'game_words'>
+      <div id = 'first' className="english_word">
+      <Bounce onAnimationEnd={extendLoad}>
+      <SavannahComponent name = {'Word to learn'}/>
+      </Bounce>
+      </div>
+      <SavannahComponent name = {'come again'}/>
+    <SavannahComponent />
+    <SavannahComponent />
+    <SavannahComponent />
+    </div>;
 
 class Savannah extends React.Component {
 
@@ -21,31 +43,56 @@ class Savannah extends React.Component {
     gameStarted: false
   }
 
-  reload() {
-    console.log('again');
-  }
+    startGame = () => {
+    this.setState({
+      gameStarted: true
+    })
+    }
+
+    exitGame = () => {
+      this.setState({
+        gameStarted: false
+      })
+      }     
 
   render() {
     const divStyle = {
       textAlign: 'center'
     }
-
-return (
-  <div className="savannah_container">
+   let structure;
+    if(this.state.gameStarted) {
+       structure = 
+      <div className="savannah_container">
     <h1>Savannah</h1>
-    <Bounce onAnimationEnd={this.reload}>
+    <button onClick={this.exitGame}>Exit</button>
+    <div id = 'first'>
+    <Bounce onAnimationEnd={extendLoad}>
     <div className="english_word">
       <SavannahComponent name = {'Word to learn'}/>
       </div>
       </Bounce>
       <div className="game_words">
-    <SavannahComponent name = {'translation'}/>
-    <SavannahComponent />
-    <SavannahComponent />
-    <SavannahComponent />
+    <SavannahComponent num = {1} name = {'translation'}/>
+    <SavannahComponent num = {2} name = {'translation2'} />
+    <SavannahComponent num = {3} name = {'translation3'} />
+    <SavannahComponent num = {4} name = {'translation4'} />
     </div>
-    
     </div>
+    </div>
+    }
+
+    if(!this.state.gameStarted) {
+      structure = 
+     <div className="savannah_container">
+   <h1>Savannah</h1>
+   <button onClick={this.startGame}>Click here to start</button>
+   </div>
+   }
+
+return (
+  <div>
+    {structure}
+  </div>
 )
 
 }
