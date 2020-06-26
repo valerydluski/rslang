02209ef185a7f788d691e55e206ec9395a37bf11
Konstantin Dedulya <eximal@gmail.com>
2020-06-26 14:49:37 +0300

    fix: fix styles

--- src/pages/homePage/styled/HomePageContainer.jsx
+++ src/pages/homePage/styled/HomePageContainer.jsx
@@ -4,7 +4,7 @@ const HomePageContainer = styled.div`
   width: 100%;
   min-height: 100%;
   display: grid;
-  grid-template-columns: 2fr 5fr 3fr;
+  grid-template-columns: 15% 60% 25%;
 `;
 
 export default HomePageContainer;
