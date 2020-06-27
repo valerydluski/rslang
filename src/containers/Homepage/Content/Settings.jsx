630d663545d28a1e9f6d554af6d27e82082caa88
valerydluski <valerydluski@gmail.com>
2020-06-27 20:50:55 +0300

    feat: add toast for checkboxes validation

--- src/containers/Homepage/Content/Settings.jsx
+++ src/containers/Homepage/Content/Settings.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { toast } from 'react-toastify';
 import ReduxSettingsForm from '../../../components/HomePage/Content/Settings/SettingsForm';
 import saveUserSettings from '../../../redux/UserSettings/actions';
 import checkboxesValidator from '../../../utils/validators/checkboxesValidator';
@@ -9,6 +10,7 @@ function SettingContent(props) {
   const { saveSettings, initialValues } = props;
   const onSubmit = (formData) => {
     if (!checkboxesValidator(formData)) {
+      toast.info('you must select at least one hint');
     } else saveSettings(formData);
   };
   return (
