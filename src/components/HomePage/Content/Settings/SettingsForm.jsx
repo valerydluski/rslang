2d29f48b70c44d8f1de08b7344e897d6a5a96337
valerydluski <valerydluski@gmail.com>
2020-06-27 11:43:49 +0300

    feat: add change count words per page for games

--- src/components/HomePage/Content/Settings/SettingsForm.jsx
+++ src/components/HomePage/Content/Settings/SettingsForm.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 import PropTypes from 'prop-types';
 import AppSettings from './AppSettings/AppSettings';
+import Input from '../../../UI/Input/Input';
+import WordsPerPageValidator from '../../../../utils/validators/wordsPerPageValidator';
 
 const SettingsForm = (props) => {
   const { handleSubmit } = props;
@@ -9,8 +11,45 @@ const SettingsForm = (props) => {
     <form onSubmit={handleSubmit}>
       <AppSettings />
       <h3>AudioCall</h3>
+      <Field
+        name="AudioCallWordsPerPage"
+        key="AudioCallWordsPerPage"
+        type="number"
+        placeholder="Audio call - words per page"
+        validate={WordsPerPageValidator}
+        parse={(val) => parseInt(val, 10)}
+        component={Input}
+      />
       <h3>Savannah</h3>
+      <Field
+        name="SavannahWordsPerPage"
+        key="SavannahWordsPerPage"
+        type="number"
+        placeholder="Savannah - words per page"
+        validate={WordsPerPageValidator}
+        parse={(val) => parseInt(val, 10)}
+        component={Input}
+      />
       <h3>SpeakIT</h3>
+      <Field
+        name="SpeakITWordsPerPage"
+        key="SpeakITWordsPerPage"
+        type="number"
+        placeholder="SpeakIT - words per page"
+        validate={WordsPerPageValidator}
+        parse={(val) => parseInt(val, 10)}
+        component={Input}
+      />
+      <h3>Sprint</h3>
+      <Field
+        name="SprintWordsPerPage"
+        key="SprintWordsPerPage"
+        type="number"
+        placeholder="Sprint - words per page"
+        validate={WordsPerPageValidator}
+        parse={(val) => parseInt(val, 10)}
+        component={Input}
+      />
       <button type="submit" aria-label="Save">
         Save
       </button>
