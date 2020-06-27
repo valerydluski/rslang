4c2aafef9bff32583e77cc38e291132ac0e8d1cc
Taras Demidovich <tarasdemidovich1995@gmail.com>
2020-06-28 01:07:59 +0300

    feat: add styles to select in addition to UI

--- src/components/UI/Select/Styled/ToggleButton.jsx
+++ src/components/UI/Select/Styled/ToggleButton.jsx
@@ -5,7 +5,20 @@ import { TOGGLE_BUTTON_HEIGHT } from './constants';
 const ToggleButton = styled.button`
   display: block;
   height: ${TOGGLE_BUTTON_HEIGHT}px;
+  margin-bottom: 2px;
   width: 100%;
+  text-align: left;
+  font-size: 10px;
+  color: #c4c4c4;
+  background-color: transparent;
+  padding: 0;
+  padding-left: 5px;
+  border: 0;
+  outline: 0;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 export default ToggleButton;
