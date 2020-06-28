5b3d7aa92fc6a26c8fe67319fb017d01cab55e3b
valerydluski <valerydluski@gmail.com>
2020-06-28 09:58:00 +0300

    feat: add service putSettings

---
+++ src/services/putSettings.js
@@ -0,0 +1,28 @@
+import { toast } from 'react-toastify';
+import { API } from '../config';
+import fetchData from '../utils/fetchData';
+
+async function putSettings(user, settings) {
+  try {
+    const {
+      URL,
+      ENDPOINTS: { USERS, SETTINGS },
+    } = API;
+
+    return await fetchData(`${URL}/${USERS}/${user.userId}/${SETTINGS}/`, {
+      method: 'PUT',
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(settings),
+    });
+  } catch (e) {
+    toast.error('Did not put settings');
+    throw new Error('Did not put settings');
+  }
+}
+
+export default putSettings;
