3159065b0a0670d60240288bf65fa9b652632ff6
valerydluski <valerydluski@gmail.com>
2020-07-04 13:40:17 +0300

    fix: fix change appMode

--- src/pages/games/Sprint/Sprint.jsx
+++ src/pages/games/Sprint/Sprint.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import GoToHomePageButton from '../../../containers/Buttons/GoHomePageButton/GoHomePageButton';
-import changeAppMode from '../../../redux/AppMode/action';
+import { changeAppMode } from '../../../redux/AppMode/action';
 import SprintContainerStyled from '../../../containers/Sprint/Styled/SprintContainerStyled';
 import SprintGameContainer from '../../../containers/Sprint/SprintGameContainer';
 import LoadingSpinner from '../../../components/LoadingSpinner/LoadingSpinner';
--- src/redux/AppMode/AppModeReducer.js
+++ src/redux/AppMode/AppModeReducer.js
@@ -1,4 +1,4 @@
-import CHANGE_APP_MODE from './types';
+import { CHANGE_APP_MODE, CHANGE_APP_MODE_LEARN } from './types';
 
 const initialState = {
   appMode: 'mainPage',
@@ -7,6 +7,8 @@ const initialState = {
 const changeAppModeReducer = (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_APP_MODE:
+      return { ...state, appMode: action.payload };
+    case CHANGE_APP_MODE_LEARN:
       return { ...state, appMode: action.payload };
     default:
       return state;
