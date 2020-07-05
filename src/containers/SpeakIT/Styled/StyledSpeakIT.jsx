566f207159a9469b4ba4bafa889d1c5322e9190a
valerydluski <valerydluski@gmail.com>
2020-07-05 11:08:36 +0300

    feat: change strucrute SpeakIT

---
+++ src/containers/SpeakIT/Styled/StyledSpeakIT.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const SpeakITContainerStyled = styled.div`
+  max-width: 1920px;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  place-items: center;
+  background: #e5e5e5;
+  box-sizing: border-box;
+`;
+
+export default SpeakITContainerStyled;
