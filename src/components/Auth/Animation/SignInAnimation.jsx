5a64b86a2fbedecd376ed02dac0f61dae8f8fc81
valerydluski <valerydluski@gmail.com>
2020-07-08 22:48:21 +0300

    fix: fix eslint problems

--- src/components/Auth/Animation/SignInAnimation.jsx
+++ src/components/Auth/Animation/SignInAnimation.jsx
@@ -1,24 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Lottie from 'react-lottie';
 import animationData from '../../../assets/animation/signin/data.json';
 
-class SignInAnimation extends Component {
-  render() {
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData,
-      rendererSettings: {
-        preserveAspectRatio: 'xMidYMid slice',
-      },
-    };
-
-    return (
-      <div>
-        <Lottie options={defaultOptions} />
-      </div>
-    );
-  }
+function SignInAnimation({ defaultOptions }) {
+  return (
+    <div>
+      <Lottie options={defaultOptions} />
+    </div>
+  );
 }
+
+SignInAnimation.propTypes = {
+  defaultOptions: PropTypes.instanceOf(Object),
+};
+
+SignInAnimation.defaultProps = {
+  defaultOptions: {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  },
+};
 
 export default SignInAnimation;
