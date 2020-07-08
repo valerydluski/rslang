a35f5ec5d3eb41339c3f18d0ba232467ab24002b
Artsemi Shareika <artsemishareika@gmail.com>
2020-07-08 19:30:44 +0300

    feat: add about us adaptive

--- src/components/HomePage/Content/AboutUs/AboutUs.jsx
+++ src/components/HomePage/Content/AboutUs/AboutUs.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Translate } from 'react-redux-i18n';
 import PropTypes from 'prop-types';
-import Title from '../../../StatusMenu/Styled/Title';
+import Title from '../../Content/Main/components/Title/styled/UserTitleContainer';
 import TitleWithUnderline from '../../../HomePage/Content/Main/components/Title/styled/TitleWithUnderline';
 import TeamContainer from '../AboutUs/components/AboutPersonCard/styled/TeamContainer';
 import AboutPersonCard from '../AboutUs/components/AboutPersonCard/AboutPersonCard'
 import ContentContainer from '../ContentContainer';
+import helloImg from '../../../../assets/img/helloImg.svg';
 
 export default function AboutUs(props) {
-  const { team, userName } = props;
+  const { userName, team } = props;
   return (
     <ContentContainer>
-      <Title userName={userName} />
+      <Title userName={userName} bg={helloImg}/>
       <TitleWithUnderline>
         <Translate value="HomePage.about" />
       </TitleWithUnderline>
--- src/components/HomePage/Content/AboutUs/components/AboutPersonCard/styled/AboutPersonCardContainer.jsx
+++ src/components/HomePage/Content/AboutUs/components/AboutPersonCard/styled/AboutPersonCardContainer.jsx
@@ -8,7 +8,7 @@ const AboutPersonCardContainer = styled.div`
     line-height: 22px;
     text-transform: uppercase;
     color: #000000;
-    margin-top: -16vh;
+    margin-top: -19vh;
     margin-left: 14.1vw;
      display: block;
      border-bottom: 1px solid #c4c4c4;
@@ -37,6 +37,22 @@ const AboutPersonCardContainer = styled.div`
     width: 170px;
   }
 
+  @media (max-width: 1295px) {
+      p, h3 {
+    margin-left: 16.1vw;
+      }
+  }
+
+  @media (max-width: 1250px) {
+    img {
+        width: 137px;
+      }
+
+      p, h3 {
+        margin-left: 14.1vw;
+          }
+  }
+
   @media (max-width: 1150px) {
     img {
     width: 137px;
@@ -47,57 +63,106 @@ const AboutPersonCardContainer = styled.div`
     h3 {
     font-size: 13px;
     height: 35px;
+    margin-top: -15vh;
     }
+
     p {
       font-size: 12px;
     }
+
+    img {
+        width: 120px;
+      }
   }
 
   @media (max-width: 950px) {
-    width: 13vw;
+    p, h3 {
+        margin-left: 16.1vw;
+          }
   }
 
   @media (max-width: 830px) {
-    width: 12vw;
+    p, h3 {
+        margin-left: 17.1vw;
+          }
   }
 
   @media (max-width: 767px) {
     width: 100%;
-    height: 75vh;
+    height: 56vh;
     h3 {
       font-size: 26px;
       height: 35px;
+       margin-left: 0;
+      margin-top: 0;
+      width: 100%;
+      text-align: center;
       }
-    p {
-        width: 70vw;
+
+      p {
+    margin-left: 0;
+    margin-top: 0;
+    width: auto;
       }
+
     span {
+        display: inline-block;
+        width: 100%;
         font-size: 19px;
         line-height: 27px;
       }
+
+      img {
+          margin-left: 41%;
+          margin-bottom: 5vh;
+      }
   }
 
-  @media (max-width: 620px) {
-    height: 65vh;
+  @media (max-width: 740px) {
+    img {
+        margin-left: 39%;
+    }
   }
+
+  @media (max-width: 600px) {
+    img {
+        margin-left: 38%;
+    }
+  }
+
+  @media (max-width: 510px) {
+    img {
+        margin-left: 36%;
+    }
+  }
+
 
   @media (max-width: 500px) {
-    width: 97%;
-    height: 55vh;
+      margin-bottom: 10vh;
   }
 
-  @media (max-width: 420px) {
-    height: 45vh;
-    width: 80vw;
+  @media (max-width: 450px) {
+    img {
+        margin-left: 33%;
+    }
   }
 
-  @media (max-width: 390px) {
-    width: 80vw;
+  @media (max-width: 425px) {
+    height: 63vh;
+}
+
+@media (max-width: 400px) {
+    img {
+        margin-left: 28%;
+    }
   }
 
-  @media (max-width: 340px) {
-    width: 77vw;
-  }
+@media (max-width: 352px) {
+    height: 68vh;
+    img {
+        margin-left: 24%;
+    }
+}
 `;
 
 export default AboutPersonCardContainer;
