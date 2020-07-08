9f22480f526a914805bbb3b67d7a09ae42c115ea
valerydluski <valerydluski@gmail.com>
2020-07-08 14:20:37 +0300

    fix: fix span

--- src/utils/validators/nameValidator.js
+++ src/utils/validators/nameValidator.js
@@ -2,7 +2,7 @@ import { toast } from 'react-toastify';
 
 export default (value) => {
   if (!value) {
-    return toast.warning('error');
+    return toast.warning('name not be empty');
   }
   return undefined;
 };
