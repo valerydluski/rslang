d7c94708c1cfa08da2b7e7d98da9967623c7025d
valerydluski <valerydluski@gmail.com>
2020-07-08 23:35:31 +0300

    feat: add animation for games page

--- src/pages/gamePage/Styled/StartGamePageStyled.jsx
+++ src/pages/gamePage/Styled/StartGamePageStyled.jsx
@@ -1,14 +1,9 @@
 import styled from 'styled-components';
-import background from '../../../assets/img/start-page-back.svg';
 
 const StartGamePageStyled = styled.div`
   width: 100%;
   height: 100%;
-  background-repeat: no-repeat;
   min-height: 100vh;
-  background-position-y: 120px;
-  background-size: cover;
-  background-image: url(${background});
   color: #000000;
 `;
 
