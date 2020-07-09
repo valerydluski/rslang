008abd8cd09f208790d6f037285a7d93f1860380
Taras Demidovich <tarasdemidovich1995@gmail.com>
2020-07-09 20:09:35 +0300

    feat: add animation to sprint

---
+++ src/components/Sprint/Styled/StyledPattern.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import { DEVICE } from '../../../config';
+
+const StyledPattern = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 400px;
+  height: 100%;
+
+  @media ${DEVICE.laptopL} {
+    width: 200px;
+  }
+
+  @media ${DEVICE.laptop} {
+    width: 100px;
+  }
+
+  @media ${DEVICE.tablet} {
+    width: 0;
+  }
+`;
+export default StyledPattern;
