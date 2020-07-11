f4a186123be98266538b93a323606baa150c126a
Artsemi Shareika <artsemishareika@gmail.com>
2020-07-11 12:24:55 +0300

    fix: central margin

--- src/pages/homePage/styled/HomePageContainer.jsx
+++ src/pages/homePage/styled/HomePageContainer.jsx
@@ -8,7 +8,7 @@ const HomePageContainer = styled.div`
   grid-template-columns: 2fr 6fr 3fr;
 
   @media ${DEVICE.laptop} {
-    grid-template-columns: 2fr 6fr 0fr;
+    grid-template-columns: 1fr 6fr 0fr;
   }
 
   @media (max-width: 767px) {
