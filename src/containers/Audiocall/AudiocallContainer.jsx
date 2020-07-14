7388d12afd9eb6d8d8d8e2d83f352ed904bf5608
Nelly Kovalenko <nelly.kovalenko.a@gmail.com>
2020-07-14 18:28:35 +0300

    hotfix: wrong words saving

--- src/containers/Audiocall/AudiocallContainer.jsx
+++ src/containers/Audiocall/AudiocallContainer.jsx
@@ -100,7 +100,7 @@ const AudioCallContainer = ({
 
   function autoSolve() {
     changegameProgressLine(gameProgressLine + 100 / wordsCollection.length);
-    addWordToWrong([...wrongAnsweredWords, wordsCollection[currentWordIndex].word]);
+    addWordToWrong([...wrongAnsweredWords, currentGameWords[currentWordIndex].word]);
     toggleWordStatus(true);
     answerResult.isCorrect = true;
     answerResult.words = additionalWords;
@@ -109,7 +109,9 @@ const AudioCallContainer = ({
 
   function processUserAnswer(isCorrect, words, selectedIndex, correctIndex) {
     changegameProgressLine(gameProgressLine + 100 / wordsCollection.length);
-    if (!isCorrect) addWordToWrong([...wrongAnsweredWords, wordsCollection[currentWordIndex].word]);
+    if (!isCorrect) {
+      addWordToWrong([...wrongAnsweredWords, currentGameWords[currentWordIndex].word]);
+    }
     answerResult = { isCorrect, words, selectedIndex, correctIndex };
     toggleWordStatus(true);
   }
