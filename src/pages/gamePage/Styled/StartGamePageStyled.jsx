5d1a1dd20ee897555127bc462fdcc1d94d5462cc
Taras Demidovich <tarasdemidovich1995@gmail.com>
2020-07-14 18:51:32 +0300

    feat: add adaptive layout

--- src/pages/gamePage/Styled/StartGamePageStyled.jsx
+++ src/pages/gamePage/Styled/StartGamePageStyled.jsx
@@ -1,10 +1,19 @@
 import styled from 'styled-components';
+import { DEVICE } from '../../../config';
 
 const StartGamePageStyled = styled.div`
   width: 100%;
   height: 100%;
   min-height: 100vh;
+  padding: 30px;
+  box-sizing: border-box;
   color: #000000;
+
+  @media ${DEVICE.laptopL} {
+    & > div:last-of-type {
+      margin-top: -200px;
+    }
+  }
 `;
 
 export default StartGamePageStyled;
